cca82fec8c3b8874b6204e8eb6023d904abceac6
syl <sysu_shaoyul@163.com>
2019-02-15 20:28:22 +0800

    提交项目

---
+++ HelloProject/Hello/src/main/java/com/itheima/Hello.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/***
+ *@ClassName:Hello
+ *@Desctiption TODO
+ *@Author:shaoyuliang
+ *@Date 2019/2/15 20:19
+ *@Version 1.0
+ ****/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
